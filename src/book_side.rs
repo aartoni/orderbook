5dc7843b386af7b3978538491b7fb153d7405caa
aartoni <arto7796@gmail.com>
2022-05-13 18:49:39 -0400

    Implement remove method for book side struct

--- src/book_side.rs
+++ src/book_side.rs
@@ -23,6 +23,20 @@ impl BookSide {
         self.prices.insert(order.price, price_level);
     }
 
+    pub fn remove(&mut self, order: Order) -> Option<Order> {
+        let mut outcome = None;
+
+        if let Some(price_level) = self.prices.get_mut(&order.price) {
+            outcome = price_level.remove(order);
+
+            if price_level.len() == 0 {
+                self.prices.remove(&order.price);
+            }
+        }
+
+        outcome
+    }
+
     pub fn min(&self) -> Option<&PriceLevel> {
         self.prices.peek()
     }
@@ -105,5 +119,30 @@ mod test {
 
         assert_eq!(side.min().unwrap().price, dec!(1.0));
         assert_eq!(side.max().unwrap().price, dec!(3.0));
+    }
+
+    #[test]
+    fn test_remove() {
+        let mut side = BookSide::new();
+        let first_order = Order::new(1, Side::Ask, Instant::now(), dec!(1.0), dec!(1.0));
+        let second_order = Order::new(1, Side::Ask, Instant::now(), dec!(2.0), dec!(2.0));
+
+        side.append(first_order);
+        side.append(second_order);
+
+        side.remove(second_order);
+
+        assert_eq!(side.prices.len(), 1);
+    }
+
+    #[test]
+    fn test_remove_last() {
+        let mut side = BookSide::new();
+        let order = Order::new(1, Side::Ask, Instant::now(), dec!(1.0), dec!(1.0));
+
+        side.append(order);
+        side.remove(order);
+
+        assert_eq!(side.prices.len(), 0);
     }
 }
