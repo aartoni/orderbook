172f0b89570d9b40c53fbc980648b3fd4c6b76f8
aartoni <arto7796@gmail.com>
2022-05-15 00:04:53 -0400

    Implement stub parse/execute threaded mechanism

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::sync::mpsc;
+use std::thread;
+
 fn main() {
-    println!("Hello, world!");
+    // Get three communication channels
+    let (to_worker, from_reader) = mpsc::channel();
+    let (to_reader, from_worker) = mpsc::channel();
+
+    // Spawn the reader thread
+    thread::spawn(move || {
+        loop {
+            to_worker.send(true).unwrap();
+            println!("Reader writes");
+            from_worker.recv().unwrap();
+        }
+    });
+
+    // Compute command received from the reader
+    while let Ok(_) = from_reader.recv() {
+        println!("Worker writes");
+        to_reader.send(()).unwrap();
+    }
 }
