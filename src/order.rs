2351c4be81ae8bff1abcc467f7a1c1c2af3d011b
aartoni <arto7796@gmail.com>
2022-05-16 00:11:59 -0400

    Add documentation for the Order type and Side enum

--- src/order.rs
+++ src/order.rs
@@ -1,14 +1,25 @@
 use std::ops::Not;
 
+/// Two possible sides of an order book, an ask indicates a sell order and a bid
+/// indicates a buy order.
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub enum Side {
     Bid,
-    Ask
+    Ask,
 }
 
 impl Not for Side {
     type Output = Self;
 
+    /// Returns the opposite side value for the enum, this method comes in handy
+    /// each time we need to check something on the other side of the order
+    /// book.
+    ///
+    /// # Example
+    /// ```
+    /// use orderbook::order::Side;
+    /// assert_eq!(!Side::Ask, Side::Bid);
+    /// ```
     fn not(self) -> Self::Output {
         if self == Self::Ask {
             Self::Bid
@@ -18,6 +29,8 @@ impl Not for Side {
     }
 }
 
+/// The order is the smallest part of the program, it is constructed by the
+/// order book on each append operation.
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Order {
     pub id: u32,
@@ -28,6 +41,8 @@ pub struct Order {
 }
 
 impl Order {
+    // The constructor is the only explicitly implemented method for the `Order`
+    // type.
     #[must_use]
     pub const fn new(id: u32, user_id: u32, side: Side, price: u32, quantity: u32) -> Self {
         Self { id, user_id, side, price, quantity }
@@ -53,5 +68,4 @@ mod tests {
         assert_eq!(order.price, price);
         assert_eq!(order.quantity, quantity);
     }
-
 }
